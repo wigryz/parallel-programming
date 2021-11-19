71a496a931cddd92d5a77df2713b0f78e4c4c786
wigryz <tomasz.ligeza3@gmail.com>
2021-11-19 09:31:04 +0100

    After lab 6

---
+++ lab_6/src/com/wigryz/lab_6/HistogramTest.java
@@ -0,0 +1,67 @@
+package com.wigryz.lab_6;
+
+import java.util.Scanner;
+
+
+class HistogramTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Set number of chars: ");
+        int NUMBER_OF_CHARS = scanner.nextInt();
+
+        System.out.println("Set image size: n (#rows), m(#kolumns)");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        Obraz obraz_1 = new Obraz(n, m, NUMBER_OF_CHARS);
+
+        obraz_1.calculate_histogram();
+        obraz_1.print_histogram();
+
+//        obraz_1.clear_histogram();
+
+        System.out.println("Set number of threads");
+        int num_threads = scanner.nextInt();
+//
+//        Watek[] NewThr = new Watek[num_threads];
+//
+//        for (int i = 0; i < num_threads; i++) {
+//            (NewThr[i] = new Watek(i, obraz_1)).start();
+//        }
+//
+//        for (int i = 0; i < num_threads; i++) {
+//            try {
+//                NewThr[i].join();
+//            } catch (InterruptedException e) {
+//            }
+//        }
+
+//        System.out.println("Czy oba histogramy sie zgadzaja? " + obraz_1.checkBothHistograms());
+
+
+        System.out.println("\n------------------------------------------\n");
+        System.out.println("DEKOMPOZYCJA HISTOGRAM\n");
+
+
+        Thread[] DecThr = new Thread[num_threads];
+
+        for (int i = 0; i < num_threads; i++) {
+            int start = NUMBER_OF_CHARS/num_threads * i;
+            int end = NUMBER_OF_CHARS/num_threads * (i + 1);
+            (DecThr[i] = new Thread(new WatekDec(start, end, obraz_1))).start();
+        }
+
+        for (int i = 0; i < num_threads; i++) {
+            try {
+                DecThr[i].join();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        System.out.println("Czy oba histogramy sie zgadzaja? " + obraz_1.checkBothHistogramsDec());
+
+    }
+
+}
+
